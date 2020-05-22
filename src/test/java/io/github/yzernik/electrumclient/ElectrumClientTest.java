a1136c961b2f3aec6e4a5fdaf7be47c779a86ba8
Jonathan Zernik <yzernik@gmail.com>
2020-05-21 23:53:10 -0700

    Use generics for subscribe headers connection class. (#6)

--- src/test/java/io/github/yzernik/electrumclient/ElectrumClientTest.java
+++ src/test/java/io/github/yzernik/electrumclient/ElectrumClientTest.java
@@ -72,8 +72,7 @@ public class ElectrumClientTest {
 
     @Test
     public void getBlockHeaderWithConnectionClass() throws Exception {
-        ElectrumClientConnection<ElectrumClientSingleLineResponse> clientConnection
-                = new GetHeaderClientConnection(ELECTRUM_HOST, ELECTRUM_PORT);
+        GetHeaderClientConnection clientConnection = new GetHeaderClientConnection(ELECTRUM_HOST, ELECTRUM_PORT);
 
         ElectrumClientSingleLineResponse response = clientConnection.makeRequest();
         String blockString = response.getLine();
@@ -85,17 +84,14 @@ public class ElectrumClientTest {
 
     @Test
     public void subscribeBlockHeadersWithConnectionClass() throws Exception {
-        ElectrumClientConnection<ElectrumClientMultiLineResponse> clientConnection
-                = new SubscribeHeadersClientConnection(ELECTRUM_HOST, ELECTRUM_PORT);
+        SubscribeHeadersClientConnection clientConnection = new SubscribeHeadersClientConnection(ELECTRUM_HOST, ELECTRUM_PORT);
 
         ElectrumClientMultiLineResponse response = clientConnection.makeRequest();
         Stream<String> blockStringStream = response.getLines();
-        System.err.println("Finding first block string from stream...");
+        String currentBlockString = blockStringStream.findFirst().get();
 
-        String firstBlockString = blockStringStream.findFirst().get();
-
-        System.out.println(firstBlockString);
-        assert firstBlockString.startsWith("{\"jsonrpc\": \"2.0\", \"result\":");
+        System.out.println(currentBlockString);
+        assert currentBlockString.startsWith("{\"jsonrpc\": \"2.0\", \"result\":");
     }
 
 }
